cef68febdcbc10fb0e16bc519094581f2cd7d77b
Zakari Badiru <zbadiru@live.co.uk>
2020-03-13 09:31:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,6 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-
 def introduction(name, language)
   puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
